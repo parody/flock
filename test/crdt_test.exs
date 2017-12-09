b63e7c17d41c5b9453ea31bd8211d1bfac75d3e8
arpunk <arpunk@fatelectron.net>
2017-12-08 23:28:15 -0500

    Styling

--- test/crdt_test.exs
+++ test/crdt_test.exs
@@ -3,38 +3,44 @@ defmodule Flock.CRDTTest do
 
   alias Flock.CRDT
 
-  test "Create CRDT" do
+  setup_all do
+    {:ok, crdt: CRDT.new()}
+  end
+
+  test "create CRDT" do
     assert CRDT.new() == %CRDT{added: [], removed: []}
   end
 
-  test "Add element to CRDT" do
-    crdt = CRDT.new()
+  test "add element to CRDT", %{crdt: crdt} do
     assert CRDT.add(crdt, :a) == %CRDT{added: [:a], removed: []}
   end
 
-  test "Fail add existing element to CRDT" do
-    crdt = CRDT.new() |> CRDT.add(:a)
-    assert CRDT.add(crdt, :a) == {:error, "Element already exists."}
+  test "fail add existing element to CRDT", %{crdt: crdt} do
+    crdt = CRDT.add(crdt, :a)
+
+    assert CRDT.add(crdt, :a) == {:error, :already_exists}
   end
 
-  test "Remove element " do
-    crdt = CRDT.new() |> CRDT.add(:a)
+  test "remove element", %{crdt: crdt} do
+    crdt = CRDT.add(crdt, :a)
+
     assert CRDT.remove(crdt, :a) == %CRDT{added: [], removed: [:a]}
   end
 
-  test "Fail remove non existent element " do
-    crdt = CRDT.new()
-    assert CRDT.remove(crdt, :a) == {:error, "Element does not exists."}
+  test "fail remove non existent element", %{crdt: crdt} do
+    assert CRDT.remove(crdt, :a) == {:error, :not_found}
   end
 
-  test "Join two CRDTs" do
-    crdt1 = CRDT.new() |> CRDT.add(:a) |> CRDT.add(:b)
-    crdt2 = CRDT.new() |> CRDT.add(:c) |> CRDT.add(:b) |> CRDT.remove(:b)
+  test "join two CRDTs", %{crdt: crdt} do
+    crdt1 = crdt |> CRDT.add(:a) |> CRDT.add(:b)
+    crdt2 = crdt |> CRDT.add(:c) |> CRDT.add(:b) |> CRDT.remove(:b)
+
     assert CRDT.join(crdt1, crdt2) == %CRDT{added: [:a, :c], removed: [:b]}
   end
 
-  test "Get a list of current elements in CRDT" do
-    crdt = %CRDT{added: [:b,:a, :c], removed: [:b]}
+  test "return a list of current elements in CRDT" do
+    crdt = %CRDT{added: [:b, :a, :c], removed: [:b]}
+
     assert CRDT.to_list(crdt) == [:a, :c]
   end
 end
